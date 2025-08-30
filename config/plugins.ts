a7784a2c0bc65eea8e6d06605dfab9d98904a829
malamjayesh <malamj001@gmail.com>
2025-08-30 10:41:57 +0530

    using cloudnary images issues fix

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,20 @@
-export default () => ({});
+// Configure Strapi upload provider to use Cloudinary via environment variables
+// Make sure to install the provider: npm install @strapi/provider-upload-cloudinary
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: '@strapi/provider-upload-cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_CLOUD_NAME'),
+        api_key: env('CLOUDINARY_API_KEY'),
+        api_secret: env('CLOUDINARY_API_SECRET'),
+        secure: env.bool('CLOUDINARY_SECURE', true),
+      },
+      // Optional: tweak action options if needed
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+});
